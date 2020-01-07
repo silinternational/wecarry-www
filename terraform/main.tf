1daee7b57052d25e3cbeac8135e73d57c2142654
Phillip <phillip.shipley@gmail.com>
2020-01-07 16:47:42 -0500

    update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 // configuration of a Hugo site. See: https://registry.terraform.io/modules/fillup/hugo-s3-cloudfront/aws/1.0.1
 module "hugosite" {
   source         = "fillup/hugo-s3-cloudfront/aws"
-  version        = "2.0.0"
+  version        = "3.0.0"
   aliases        = ["${var.aliases}"]
   aws_region     = "${var.aws_region}"
   bucket_name    = "${var.bucket_name}"
@@ -10,20 +10,7 @@ module "hugosite" {
   cf_default_ttl = "0"
   cf_max_ttl     = "0"
   s3_origin_id   = "www-wecarry-s3-origin"
-}
-
-// Give Codeship user permission to deploy site to S3
-data "template_file" "ui-policy" {
-  template = "${file("${path.module}/bucket-policy.json")}"
-
-  vars {
-    bucket_name = "${var.cloudflare_subdomain}.${var.cloudflare_domain}"
-  }
-}
-
-resource "aws_iam_user_policy" "codeship-ui" {
-  policy = "${data.template_file.ui-policy.rendered}"
-  user   = "${data.terraform_remote_state.common.codeship_username}"
+  deployment_user_arn = "${data.terraform_remote_state.common.codeship_arn}"
 }
 
 // Create DNS CNAME record on Cloudflare
