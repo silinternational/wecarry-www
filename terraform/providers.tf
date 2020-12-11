86a3965dd4c08be3fcd7750fa9d8a30b9129fa30
Schparky <3172830+Schparky@users.noreply.github.com>
2020-12-11 10:34:06 -0700

    upgrade terraform to 0.13, cloudflare provider to 2.0

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,20 +1,12 @@
 provider "aws" {
-  version    = "~> 2.70"
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 provider "cloudflare" {
-  version = "~> 1.0"
-  email   = var.cloudflare_email
-  token   = var.cloudflare_token
-}
-
-provider "template" {
-  version = "~> 2.2"
-}
-
-provider "null" {
-  version = "~> 3.0"
+  email      = var.cloudflare_email
+  api_key    = var.cloudflare_api_key
+  api_token  = var.cloudflare_token
+  account_id = var.cloudflare_account_id
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,22 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      version = "~> 2.70"
+      source  = "hashicorp/aws"
+    }
+    cloudflare = {
+      version = "~> 2.0"
+      source  = "cloudflare/cloudflare"
+    }
+    null = {
+      version = "~> 3.0"
+      source  = "hashicorp/null"
+    }
+    template = {
+      version = "~> 2.2"
+      source  = "hashicorp/template"
+    }
+  }
 }
