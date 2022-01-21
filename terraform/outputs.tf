2876b31801fc4690a8c8c6bb967ac7f8f7631bc6
Schparky <3172830+Schparky@users.noreply.github.com>
2022-01-21 14:11:45 -0700

    remove terraform config

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "cloudfront_hostname" {
-  value       = module.hugosite.cloudfront_hostname
-  description = "CloudFront DNS hostname to create a CNAME to with DNS provider"
-}
-
--- terraform/remote.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "common" {
-  backend = "atlas"
-
-  config = {
-    name = var.tf_remote_common
-  }
-}
-
